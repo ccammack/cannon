8b3a4298517746f8f5b5fa6d0128d95948edd3f0
ccammack <clc1024@hotmail.com>
2022-12-24 10:06:28 -0800

    Clean code. Move config into global variable.

--- main.go
+++ main.go
@@ -2,37 +2,13 @@ package main
 
 import (
         "fmt"
-        //"os"
         "log"
-        //"path/filepath"
         "io/ioutil"
 
         "gopkg.in/yaml.v3"
 )
 
-
-var data = `
-file_conversion_rules:
-  -
-    type: extension
-    matches: [mp3]
-    tag: blabla-hostname
-  -
-    type: extension
-    matches: [rule1]
-    command: blablabla-host
-    tag: blabla-hostname
-  -
-    type: extension
-    matches: [mp4, jpg]
-    command: ls -alg
-    tag: video
-  -
-    type: mime
-    matches: [application/text]
-    command: more t.txt
-    tag: head
-`
+var config = T{}
 
 type T struct {
 	FileConversionRules []struct {
@@ -44,31 +20,20 @@ type T struct {
 }
 
 func get_config_path() string {
-        // ex, err := os.Executable()
-        // if err != nil {
-        //         panic(err)
-        // }
-        // exPath := filepath.Dir(ex)
-        // fmt.Println(exPath)
-        // return exPath
         return "/home/ccammack/work/cannon/cannon.yml"
 }
 
 func load_config() {
-        configPath := get_config_path()
-        configData, err := ioutil.ReadFile(configPath)
+        data, err := ioutil.ReadFile(get_config_path())
         if err != nil {
-                log.Printf("configData.Get err   #%v ", err)
+                log.Fatalf("data.Get err #%v", err)
         }
 
-        t := T{}
-
-        //err := yaml.Unmarshal([]byte(data), &t)
-        err = yaml.Unmarshal([]byte(configData), &t)
+        err = yaml.Unmarshal([]byte(data), &config)
         if err != nil {
                 log.Fatalf("error: %v", err)
         }
-        fmt.Printf("--- t:\n%v\n\n", t)
+        fmt.Printf("--- t:\n%v\n\n", config)
 }
 
 func main() {
