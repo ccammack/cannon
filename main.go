3df94f17735868cca994724fa0f3103d46eb5a50
ccammack <clc1024@hotmail.com>
2022-12-24 09:39:38 -0800

    Load YAML from a file.

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
         "fmt"
+        //"os"
         "log"
+        //"path/filepath"
+        "io/ioutil"
 
         "gopkg.in/yaml.v3"
 )
@@ -40,14 +43,35 @@ type T struct {
 	} `yaml:"file_conversion_rules"`
 }
 
-func main() {
+func get_config_path() string {
+        // ex, err := os.Executable()
+        // if err != nil {
+        //         panic(err)
+        // }
+        // exPath := filepath.Dir(ex)
+        // fmt.Println(exPath)
+        // return exPath
+        return "/home/ccammack/work/cannon/cannon.yml"
+}
+
+func load_config() {
+        configPath := get_config_path()
+        configData, err := ioutil.ReadFile(configPath)
+        if err != nil {
+                log.Printf("configData.Get err   #%v ", err)
+        }
+
         t := T{}
 
-        err := yaml.Unmarshal([]byte(data), &t)
+        //err := yaml.Unmarshal([]byte(data), &t)
+        err = yaml.Unmarshal([]byte(configData), &t)
         if err != nil {
                 log.Fatalf("error: %v", err)
         }
         fmt.Printf("--- t:\n%v\n\n", t)
+}
 
+func main() {
+        load_config()
 }
 
