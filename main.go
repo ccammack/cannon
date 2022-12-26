6de6e215a485d4f5ce4946ae158b006f37d0da40
ccammack <clc1024@hotmail.com>
2022-12-25 18:44:52 -0800

    Get viper reload working properly.

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
   "log"
-  "os"
   "fmt"
   "sync"
   "github.com/spf13/viper"
   "github.com/fsnotify/fsnotify"
-//  "github.com/mitchellh/mapstructure"
 )
 
 var (
@@ -17,11 +15,11 @@ var (
 
 type Config struct {
   FileConversionRules []struct {
-    Type    string   `yaml:"type"`
-    Matches []string `yaml:"matches"`
-    Command string   `yaml:"command,omitempty"`
-    Tag     string   `yaml:"tag"`
-  } `yaml:"file_conversion_rules"`
+    Type    string   `mapstructure:"type"`
+    Matches []string `mapstructure:"matches"`
+    Command string   `mapstructure:"command,omitempty"`
+    Tag     string   `mapstructure:"tag"`
+  } `mapstructure:"file_conversion_rules"`
 }
 
 func GetConfig() *Config {
@@ -33,35 +31,22 @@ func GetConfig() *Config {
 func loadConfig() {
   if err := viper.ReadInConfig(); err != nil {
     log.Println("open config: ", err)
-    os.Exit(1)
+    panic(err)
   }
-  fmt.Println(viper.AllSettings())
   temp := new(Config)
   if err := viper.Unmarshal(&temp); err != nil {
-  //if err := mapstructure.Decode(viper.AllSettings(), &temp); err != nil {
-  //if err := mapstructure.Decode(viper.GetStringMapString("file_conversion_rules"), temp); err != nil {
-  //if err := mapstructure.Decode(viper.GetStringMap("file_conversion_rules"), temp); err != nil {
-  //if err := mapstructure.Decode(viper.Get("file_conversion_rules"), temp); err != nil {
-  //if err := mapstructure.Decode(viper.GetStringSlice("file_conversion_rules"), temp); err != nil {
-      fmt.Println(err)
       log.Println("parse config: ", err)
-      os.Exit(1)
+      panic(err)
   }
-
-  fmt.Println(temp)
-  fmt.Printf("--- t:\n%v\n\n", temp)
   configLock.Lock()
   config = temp
-  fmt.Println(config)
-  fmt.Printf("--- t:\n%v\n\n", config)
   configLock.Unlock()
-  fmt.Println(viper.AllSettings())
+  fmt.Println(config)
 }
 
 func init() {
   viper.SetConfigType("yaml")
   viper.SetConfigName("config")
-  viper.AddConfigPath("/home/ccammack/work/cannon")
   viper.AddConfigPath(".")
   viper.OnConfigChange(func(e fsnotify.Event) {
     fmt.Println("Config file changed:", e.Name)
